5b3218d168fd5706a516c1aa73245df3294f565a
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-09 15:10:21 +0900

    contentsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_045750) do
+ActiveRecord::Schema.define(version: 2021_05_09_055520) do
+
+  create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "target_name", null: false
+    t.text "target_info", null: false
+    t.date "target_date", null: false
+    t.integer "study_time", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_05_09_045750) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "users"
 end
