a0a3417e6ad6686de1623a634575f1d27e534558
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-09 19:36:38 +0900

    new.htmlの作成

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,23 @@
 class ContentsController < ApplicationController
   def index
   end
+
+  def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.new(content_params)
+    if @content.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def content_params
+    params.require(:content).permit(:target_name, :target_info, :target_date, :study_time).merge(user_id: current_user.id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'contents/indes'
   root to: "contents#index"
+  resources :contents, only: [:index, :new, :create]
 end
