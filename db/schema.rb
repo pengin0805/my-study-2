68ba553e4ce35a585b22d912d638ed2ad755e0d5
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-05-10 15:41:59 +0900

    DBの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_055520) do
+ActiveRecord::Schema.define(version: 2021_05_10_051725) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "text", null: false
+    t.bigint "user_id"
+    t.bigint "content_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_comments_on_content_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "target_name", null: false
@@ -36,5 +46,7 @@ ActiveRecord::Schema.define(version: 2021_05_09_055520) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "contents"
+  add_foreign_key "comments", "users"
   add_foreign_key "contents", "users"
 end
